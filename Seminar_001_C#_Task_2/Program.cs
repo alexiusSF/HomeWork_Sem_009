8a9c16c13ffe6bebf22141488c81521eb00d233c
Alexius <i@alexiussf.ru>
2022-06-27 21:32:34 +0300

    Seminar_001_C#_Task_2 РЕШЕНИЕ

---
+++ Seminar_001_C#_Task_2/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    Console.WriteLine(b);
+}
