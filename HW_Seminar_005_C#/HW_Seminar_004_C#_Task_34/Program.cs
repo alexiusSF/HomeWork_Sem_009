a0b61ac5d53db7ab76bc7f56a4c998ecee37ce80
Alexius <i@alexiussf.ru>
2022-07-15 14:22:24 +0300

    HW_Seminar_004_C#_Task_34 РЕШЕНИЕ

---
+++ HW_Seminar_005_C#/HW_Seminar_004_C#_Task_34/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача № 34:
+// Задайте массив заполненный случайными положительными трехзначными числами.
+// Напишите программу, которая покажет количество четных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int size = InputInt("Введите размер массива: ");
+int[] numbers = new int[size];
+int sumPositive = 0;
+int count = 1;
+
+FillArray(numbers);
+PrintArray(numbers);
+
+void FillArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++) // проходимциклом по всему массиву
+    {
+        array[i] = new Random().Next(99, 999);
+    } 
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++) // метод, который выводит массив
+    {
+        Console.Write(array[i] + " ");
+    } 
+    Console.WriteLine();
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+for(int i = 0; i < numbers.Length; i++)
+{
+    if(numbers[i] % 2 == 0)
+    {
+        sumPositive += count;
+        count++;
+    }
+}
+
+Console.WriteLine($"Количество четных чисел равна: {sumPositive}");
