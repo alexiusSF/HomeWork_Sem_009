6ae35c8b43d207d23a510b8d642f0b94f13ceeb7
Alexius <i@alexiussf.ru>
2022-07-19 13:10:20 +0300

    HW_Seminar_006_C# РЕШЕНИЕ

---
+++ HW_Seminar_006_C#/HW_Seminar_006_C#_Task_43/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача № 43:
+// Напишите программу, которая найдет точку пресечения двух прямых, заданных уравнениями
+// y = k1 * x + b1, y = k2 * x + b2
+// Значения b1, k1, b2, k2 задаются пользователем.
+
+Console.Write("введите b1: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите k1: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите b2: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите k2: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"Точка пересечения двух прямых: X: {x} и Y: {y}");
