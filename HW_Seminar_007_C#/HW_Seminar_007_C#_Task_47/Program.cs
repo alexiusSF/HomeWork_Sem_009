bece3919eaf92aba179abfada8a1c2be302ba4ad
Alexius <i@alexiussf.ru>
2022-07-20 11:28:39 +0300

    HW_Seminar_007_C# РЕШЕНИЕ

---
+++ HW_Seminar_007_C#/HW_Seminar_007_C#_Task_47/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача № 47
+// Задайте двумерный массив размером MxN, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+int m = InputInt("Введите количество строк: ");
+int n = InputInt("Введите количество столбцов: ");
+double[,] numbers = new double[m, n];
+FillArray(numbers);
+PrintArray(numbers);
+
+void PrintArray(double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(new Random().Next(-100, 1000)) / 100;
+        }
+    }
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
---
+++ HW_Seminar_007_C#/HW_Seminar_007_C#_Task_50/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача № 50
+// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+// и возвращает значение этого элемента или же указание, что такого элемента нет.
+// Во вводе первая цифра - номер строки, вторая - столбца. Цифры не больше 9.
+// Например задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4 
+// 17 -> такого числа в массиве нет
+
+int m = InputInt("Введите количество строк: ");
+int n = InputInt("Введите количество столбцов: ");
+int[,] numbers = new int[9, 9];
+FillArray(numbers);
+PrintArray(numbers);
+
+if (m > numbers.GetLength(0) || n > numbers.GetLength(1))
+{
+    Console.WriteLine("такого элемента нет");
+}
+else
+{
+    Console.WriteLine($"Значение элемента {m}{n} равна -> {numbers[m-1,n-1]}");
+}
+
+void PrintArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] array)
+{
+    for(int i = 0; i < array.GetLength(0); i++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return Convert.ToInt32(Console.ReadLine());
+}
