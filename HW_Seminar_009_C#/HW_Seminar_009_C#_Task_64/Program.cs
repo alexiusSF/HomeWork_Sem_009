b7003d2170f25e314c6f67f07a2588b5e5e59be0
Alexius <i@alexiussf.ru>
2022-07-25 13:37:12 +0300

    HW_Seminar_009_C# РЕШЕНИЕ

---
+++ HW_Seminar_009_C#/HW_Seminar_009_C#_Task_64/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача № 64:
+// Задайте значения M и N.
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6 ,7, 8"
+
+Console.Clear();
+
+int n = InputInt("Введите натуральное число: ");
+int m = 1;
+if (n < 1)
+{
+    Console.WriteLine("Ввели не натуральное число: ");
+}
+Console.WriteLine(NaturalNumber(n, m));
+
+int NaturalNumber(int n, int m)
+{
+    if (n == m)
+        return n;
+    else
+        Console.Write($"{NaturalNumber(n, m + 1)}, ");
+    return m;
+}
+
+int InputInt(string output)
+{
+    Console.Write(output);
+    return int.Parse(Console.ReadLine());
+}
